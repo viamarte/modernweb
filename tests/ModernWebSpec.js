533ae7d112ba6f83b3eef48f1b8517b7cbc59089
Matheus Kautzmann <kautzmann5@gmail.com>
2014-06-13 08:00:56 -0300

    Fix #11

--- tests/ModernWebSpec.js
+++
@@ -1,7 +0,0 @@
-describe('ModernWeb', function() {
-  return describe('Detection framework', function() {
-    return it('should be able to detect when a browser support Canvas (HTML5)', function() {
-      return expect(ModernWeb.html5Compatible()).to.equal(true);
-    });
-  });
-});
